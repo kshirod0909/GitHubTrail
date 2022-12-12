dcab28bfffd22fa3e6120129f846ad4e529503fa
kshirod0909 <120356145+kshirod0909@users.noreply.github.com>
2022-12-12 15:34:21 +0530

    Add project files.

---
+++ GitHubTrail/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitHubTrail.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
